92bf2a48c13f673367a27cd9c9a027a8e926481a
Brent Smith <brents434@gmail.com>
2026-09-07 13:30:24 -0400

    revert project card description alignment back to center

--- components/Card.tsx
+++ components/Card.tsx
@@ -36,7 +36,7 @@ const Card = ({ title, description, imgSrc, slug, demoHref, dashboardHref, githu
             title
           )}
         </h2>
-        <p className="prose text-forest-text-light dark:text-forest-text-dark mb-3 max-w-none text-left">
+        <p className="prose text-forest-text-light dark:text-forest-text-dark mb-3 max-w-none">
           {description}
         </p>
         <div className="flex flex-wrap items-center justify-center gap-6">
